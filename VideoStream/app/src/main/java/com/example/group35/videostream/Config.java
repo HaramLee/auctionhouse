1cb12f35831b5258b112e15f6e2be36a422b5dd6
haram <haram@hotmail.ca>
2016-04-28 10:34:50 +0200

    Added youtube

---
+++ VideoStream/app/src/main/java/com/example/group35/videostream/Config.java
@@ -0,0 +1,13 @@
+package com.example.group35.videostream;
+
+/**
+ * Created by Haram on 2016-04-27.
+ */
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyC3nWK4O5q4rzvWhcGO7FHktvS4CdmHvaQ";
+
+}
