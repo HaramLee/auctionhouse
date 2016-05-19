4edd0234b8b16b2165e9438043864d6b3f611ec7
haram <haram@hotmail.ca>
2016-05-19 06:45:49 +0200

    asdasdasdasdad

--- eLiveLink/app/src/main/java/com/example/group35/elivelink/MainActivity.java
+++ eLiveLink/app/src/main/java/com/example/group35/elivelink/MainActivity.java
@@ -4,15 +4,9 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -25,15 +19,12 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayer.Provider;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener  {
@@ -67,6 +58,7 @@ public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.O
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
